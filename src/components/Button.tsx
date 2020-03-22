bd3f0e00c55ce5a213c9fdd540549e8773c05d38
Gabriel Brunichaki <6abriel.dev@gmail.com>
2020-03-21 22:15:22 -0300

    Base button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.button`
+  border-radius: 5px;
+  border: 0;
+  font: inherit;
+  cursor: pointer;
+`;
+
+export type Props = {
+  onClick: () => void;
+  label: string;
+}
+
+const Button: React.FC<Props> = ({ onClick, label, ...rest }) => (
+  <Container onClick={onClick} {...rest}>
+    {label}
+  </Container>
+);
+
+export default Button;
